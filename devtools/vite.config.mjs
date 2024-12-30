0a77d9442675973f3579aea3fb00627aa5debb4c
voluntas <nakai@shiguredo.jp>
2024-12-30 12:28:25 +0900

    コード整理

--- devtools/vite.config.mjs
+++ devtools/vite.config.mjs
@@ -2,17 +2,10 @@ import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: resolve(__dirname),
+  root: resolve(__dirname, 'src'),
   resolve: {
     alias: {
       '@open-ayame/ayame-web-sdk': resolve(__dirname, '../dist/ayame.mjs'),
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        index: resolve(__dirname, 'index.html'),
-      },
     },
   },
   envDir: resolve(__dirname, '..'),
